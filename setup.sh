f30d8d6ef5f8b9affc69963525bd6e99ff1fc946
sven <nate25381@gmail.com>
2020-10-11 13:02:48 -0400

    removed files that weren't needed and created install scripts

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# copy files over
+cp -r alacritty i3 i3status polybar ~/.config
+cp -r fonts/* ~/.fonts
+cp -r Wallpapers ~/Pictures
+
+# set up zsh
+sudo pacman -S zsh zsh-syntax-highlighting zsh-theme-powerlevel10k
+cp zsh/.zshrc ~/
+
+# set up Vim
+cd vim
+chmod +x vimConfig.sh
+bash vimConfig.sh
