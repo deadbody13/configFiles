408290a0d61b76cae7a626a6425eb8439add4de6
deadbody13 <nate25381@gmail.com>
2020-11-08 16:52:02 -0500

    updated packages installed and fonts

--- setup.sh
+++ setup.sh
@@ -1,27 +1,38 @@
+#! /bin/bash
+# only execute if user is setup
+# otherwise, prompts root to create it
 if [[ -e /home/sven ]]
 then
-    # basic packages
-    sudo pacman -S i3-gaps rofi vscode
+    # graphics
+    sudo pacman -S nvidia xorg
 
-    # yay
+    # basic packages
+    sudo pacman -S i3-gaps rofi vscode feh dunst picom dolphin xfce4-power-manager alacritty flameshot pulseaudio
+
+    # install yay
     pacman -S --needed git base-devel
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
 
-    # install yay
+    # install yay packages
     yay -S polybar ly
+    # enable display manager
+    systemctl enable ly
 
     # systemctl
     sudo systemctl enable ly
 
     # copy files over
-    cp -r alacritty i3 i3status polybar ~/.config
+    cp -r alacritty i3 i3status polybar dunst  ~/.config
+    # if .fonts exists, copies fonts
+    # else creates .fonts and copies fonts
     if [[ -e /home/sven/.fonts ]]
+    then
         cp -r fonts/* ~/.fonts
-        else
-            mkdir /home/sven/.fonts
-            cp -r fonts/* ~/.fonts
+    else
+        mkdir /home/sven/.fonts
+        cp -r fonts/* ~/.fonts
     fi
     cp -r Wallpapers ~/Pictures
 
@@ -33,15 +44,14 @@ then
     pacman -S --needed git base-devel
     # awesome Vim
     git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh 
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-    # yay
-    sudo systemctl enable 
-    cd yay
-    makepkg -si
+    # vim
     cd vim
     chmod +x vimConfig.sh
     bash vimConfig.sh
 else
-    echo "\nPlease create user\n"
+    echo ""
+    echo "Please create user"
+    echo ""
 fi
